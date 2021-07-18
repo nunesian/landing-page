a752779656fb864c1a1b0c5d6e465168f9cc256b
Ian Nunes <ian.nunes70@gmail.com>
2021-07-18 19:43:27 -0300

    testes finais

--- scripts/easterEgg1.js
+++ scripts/easterEgg1.js
@@ -1,7 +1,10 @@
+let count=0;
+
 function coin(){
+
     const superMario = document.getElementById("super-mario")
     const coinSound = document.getElementById("coin-sound")
-    coinSound.play()
-    setTimeout(() => {},1000)
     superMario.innerHTML = `<p id="super-mario" style="color: rgb(255, 233, 34);">Super Mario World<br>R$ 42,00</br></p>`
+    
+    coinSound.play()
 }
--- scripts/easterEgg2.js
+++ scripts/easterEgg2.js
@@ -5,7 +5,7 @@ function toasty(){
     const toasty = document.getElementById("toasty")
     easterEgg.innerHTML = `<div id="punch-button">
     <div><span>Punch Price</span></div>
-    <img src="./images/punch.png" alt="Easter Egg" onclick="toasty()"/>
+    <img src="./images/punch.png" alt="Easter Egg"/>
 </div>`    
     text.innerHTML = `<p class="description" id="mortal-kombat-price" style="color: red">Mortal Kombat: <br>R$ 55,00</br></p>`
     toasty.innerHTML = `<div id="toasty"><img src="./images/toasty.png" width=100 height=100/></div>`
